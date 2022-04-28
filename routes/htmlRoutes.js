bd01ac276c42d25314fe0b174b5e2880a8435915
gpay <gpaytakov@gmail.com>
2022-04-28 00:33:37 -0500

    get requests created and module export done

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,19 @@
 const path = require('path');
 const router = require('express').Router();
+
+// routes for index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+// routes for notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+})
+
+// if route is not specified, direct index.html file
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+module.exports = router;
