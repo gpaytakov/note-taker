27a2b201891b4ace3bbc7adb119c82c7bb962507
gpay <gpaytakov@gmail.com>
2022-04-28 00:20:42 -0500

    get and post routes created

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,3 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
 const notes = require('../db/db.json');
+
+// GET request is done for all notes
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+// POST request for new note
+router.post('/notes', (req, res) => {
+    const pathToJson = path.join(__dirname, '../db/db.json');
+    let newNote = req.body;
+    
+    notes.push(newNote);
+    
+    fs.writeFile(pathToJson, JSON.stringify(notes), error => {
+        if (error) throw error;
+        console.log('Success! New note is added.')
+    })
+    return res.json(newNote);
+})
+
+module.exports = router;
